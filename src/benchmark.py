a703c357fc5a584d82f8ac93c7bc978837b19734
Jon Sowman <jon@hexoc.com>
2014-01-06 22:47:06 +0000

    Timing for various n

--- src/benchmark.py
+++ src/benchmark.py
@@ -1,6 +1,7 @@
 # SESG6025 Benchmarking
 # Jon Sowman 2013
 import multi
+import timeit
 
 class args():
     n = 3
@@ -8,4 +9,12 @@ class args():
     its = 10000
     exercises = [1,2,3,4]
 
-multi.run_exercises(args.n, args.its, args.verbosity, args.exercises)
+def run_all():
+    multi.run_exercises(args.n, args.its, args.verbosity, args.exercises)
+
+if __name__ == '__main__':
+    for n in range(1, 10):
+        t = (n * 2) + 1
+        args.n = t
+        mtime = min(timeit.Timer(run_all).repeat(1, 1))
+        print("n = %d, time was %f secs" % (t, mtime))
