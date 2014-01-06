f9fae50905bfc6f481677a156458e33840c1366f
Jon Sowman <jon@hexoc.com>
2014-01-06 23:09:10 +0000

    Nicer plotting, should be good for graph generation for report now

--- src/benchmark.py
+++ src/benchmark.py
@@ -9,28 +9,25 @@ class args():
     n = 3
     verbosity = 0
     its = 10000
-    exercises = [1]
+    exercises = [3]
 
 def run_all():
     multi.run_exercises(args.n, args.its, args.verbosity, args.exercises)
 
 if __name__ == '__main__':
     # How many values of n (from 1, then 3, 5, 7...) should we test?
-    n = 20
+    n = 55
 
     results = numpy.array([])
     
-    for n in range(1, n):
-        t = (n * 2) + 1
-        print("Benchmarking for n=%d" % t)
-        args.n = t
+    for n in range(1, n, 2):
+        print("Benchmarking for n=%d" % n)
+        args.n = n
         mtime = min(timeit.Timer(run_all).repeat(5, 1))
-        print("%f" % mtime)
         results = numpy.append(results, mtime)
 
-    print results
     plt.plot(results)
     plt.xlabel("Grid size N")
     plt.ylabel("Run time (seconds)")
-    plt.title("Execution time for exercise 1")
+    plt.title("Execution time for exercise %d vs grid size" % args.exercises[0])
     plt.show()
